8e4a490b2bdaf488775da607e407124a7bbe9583
ggkk3979-eng <ggkk3979@gmail.com>
2026-01-09 05:29:42 +0800

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,26 @@
+// 监听请求，返回HTML页面
+export default {
+  async fetch(request) {
+    const html = `
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>i potato you</title>
+</head>
+<body>
+  <h1>i potato you 🥔❤️</h1>
+  <p>For you.</p>
+</body>
+</html>
+    `;
+    // 返回HTML响应
+    return new Response(html, {
+      headers: {
+        "Content-Type": "text/html; charset=utf-8"
+      }
+    });
+  }
+};
+
